a0ea7690dcc12fd77fb772b20e44e5cf2b83dbf6
sepefeets <rosbot@outlook.com>
2016-06-11 00:06:43 -0700

    120.0SE

--- HR/Program.cs
+++ HR/Program.cs
@@ -1166,7 +1166,7 @@ namespace HREngine.Bots
 
     public class Silverfish
     {
-        public string versionnumber = "119.0SE";
+        public string versionnumber = "120.0SE";
         private bool singleLog = false;
         private string botbehave = "rush";
         public bool waitingForSilver = false;
--- external process/Program.cs
+++ external process/Program.cs
@@ -116,7 +116,7 @@ namespace HREngine.Bots
 
     public class Silverfish
     {
-        public string versionnumber = "119.0SE";
+        public string versionnumber = "120.0SE";
         private bool singleLog = false;
         private string botbehave = "rush";
 
