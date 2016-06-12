3c61c21d42c22acfd20475d336258da9e1d1793f
sepefeets <rosbot@outlook.com>
2016-06-12 12:52:10 -0700

    undo HR 576 placement changes, they were correct before

--- cards/Sim_EX1_093.cs
+++ cards/Sim_EX1_093.cs
@@ -4,26 +4,23 @@ using System.Text;
 
 namespace HREngine.Bots
 {
-    class Sim_EX1_093 : SimTemplate //* defenderofargus
-    {
-        //Battlecry: Give adjacent minions +1/+1 and Taunt.
+	class Sim_EX1_093 : SimTemplate //defenderofargus
+	{
 
-        public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
-        {
+//    kampfschrei:/ verleiht benachbarten dienern +1/+1 und spott/.
+		public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
+		{
             List<Minion> temp = (own.own) ? p.ownMinions : p.enemyMinions;
             foreach (Minion m in temp)
             {
-                if (m.zonepos == own.zonepos - 1 || m.zonepos == own.zonepos + 1)
+                if (m.zonepos == own.zonepos - 1 || m.zonepos == own.zonepos)//position and position -1 because its not placed jet
                 {
+                    m.taunt = true;
                     p.minionGetBuffed(m, 1, 1);
-                    if (!m.taunt)
-                    {
-                        m.taunt = true;
-                        if (m.own) p.anzOwnTaunt++;
-                        else p.anzEnemyTaunt++;
-                    }
                 }
             }
-        }
-    }
+		}
+
+
+	}
 }
