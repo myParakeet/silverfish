5f1ac53325b8dd806c56b8867e3e2f5da0fa7412
sepefeets <rosbot@outlook.com>
2016-07-07 01:43:48 -0700

    Revert "Simplify polymorph and hex penalties"
    This reverts commit e4147e1a816a8a5403efde31bf7d1c936f2a82b0.

--- Penalties/Pen_CS2_022.cs
+++ Penalties/Pen_CS2_022.cs
@@ -8,14 +8,7 @@ namespace HREngine.Bots
 	{
 		public override float getPlayPenalty(Playfield p, Handmanager.Handcard hc, Minion target, int choice, bool isLethal)
 		{
-            if (target.own)
-            {
-                return 500; //todo parakeet; is this necessary? allow polymorph own if advantagous?
-            }
-            else
-            {
-                return 16; //cardvalue; don't waste on small threats
-            }
-        }
+			return 0;
+		}
 	}
 }
