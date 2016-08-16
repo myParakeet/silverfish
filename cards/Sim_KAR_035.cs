9654e363a27cf12810df4ba4373096e60c11e8d0
sepefeets <rosbot@outlook.com>
2016-08-16 01:02:24 -0700

    priest of the feast sim from joy's SF

--- cards/Sim_KAR_035.cs
+++ cards/Sim_KAR_035.cs
@@ -7,10 +7,10 @@ namespace HREngine.Bots
     class Sim_KAR_035 : SimTemplate //* Priest of the Feast
     {
         //Whenever you cast a spell, restore 3 Health to your hero.
-
-        public override void onCardIsGoingToBePlayed(Playfield p, Handmanager.Handcard hc, bool wasOwnCard, Minion triggerEffectMinion)
+        
+        public override void onCardIsGoingToBePlayed(Playfield p, CardDB.Card c, bool wasOwnCard, Minion triggerEffectMinion, Minion target, int choice)
         {
-            if (triggerEffectMinion.own == wasOwnCard && hc.card.type == CardDB.cardtype.SPELL)
+            if (triggerEffectMinion.own == wasOwnCard && c.type == CardDB.cardtype.SPELL)
             {
                 int heal = (wasOwnCard) ? p.getMinionHeal(3) : p.getEnemyMinionHeal(3);
                 p.minionGetDamageOrHeal(wasOwnCard ? p.ownHero : p.enemyHero, -heal);
