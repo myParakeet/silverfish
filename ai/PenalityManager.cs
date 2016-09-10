a353e118505171cf74c3f09e690ff7422697baa1
sepefeets <rosbot@outlook.com>
2016-09-10 04:49:17 -0700

    [917.3,919] don't penalize playing battle rage after other actions

--- ai/PenalityManager.cs
+++ ai/PenalityManager.cs
@@ -1110,6 +1110,8 @@ namespace HREngine.Bots
 
             if (p.owncards.Count + carddraw > 10) return 15 * (p.owncards.Count + carddraw - 10);
             if (p.owncards.Count + p.cardsPlayedThisTurn > 7) return (5 * carddraw) + 1;
+            
+            if (name == CardDB.cardName.battlerage) return -carddraw - p.optionsPlayedThisTurn + p.ownMaxMana - p.mana;
 
             return -carddraw + 2 * p.optionsPlayedThisTurn + p.ownMaxMana - p.mana;
             /*pen = -carddraw + p.ownMaxMana - p.mana;
