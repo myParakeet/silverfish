be882b9510f1165828c1f5114066818e40afb238
sepefeets <rosbot@outlook.com>
2016-09-15 16:54:10 -0700

    [968] fix playing quickshot last to really be last not 2nd last

--- ai/PenalityManager.cs
+++ ai/PenalityManager.cs
@@ -1034,7 +1034,7 @@ namespace HREngine.Bots
             if (name == CardDB.cardName.wrath && choice != 2) return 0;
             if (name == CardDB.cardName.nourish && choice != 2) return 0;
             if (name == CardDB.cardName.grovetender && choice != 2) return 0;
-            if (name == CardDB.cardName.quickshot && p.owncards.Count != 1) return 0;
+            if (name == CardDB.cardName.quickshot && p.owncards.Count != 0) return 0;
 
             int carddraw = cardDrawBattleCryDatabase[name];
             if (name == CardDB.cardName.harrisonjones)
