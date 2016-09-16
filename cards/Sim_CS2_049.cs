36748b7a48e81d6fc7236d690706eb031906e1ff
sepefeets <rosbot@outlook.com>
2016-09-15 18:02:44 -0700

    [974.1.1] tweak totemic call sim some more so that it isn't overvalued

--- cards/Sim_CS2_049.cs
+++ cards/Sim_CS2_049.cs
@@ -12,7 +12,7 @@ namespace HREngine.Bots
         CardDB.Card kid2 = CardDB.Instance.getCardDataFromID(CardDB.cardIDEnum.CS2_052);//spellpower
         CardDB.Card kid3heal = CardDB.Instance.getCardDataFromID(CardDB.cardIDEnum.NEW1_009);//healing
         CardDB.Card kid4taunt = CardDB.Instance.getCardDataFromID(CardDB.cardIDEnum.CS2_051);//stoneclaw
-        CardDB.Card kid6 = CardDB.Instance.getCardDataFromID(CardDB.cardIDEnum.EX1_575);//mana tide
+        //CardDB.Card kid6 = CardDB.Instance.getCardDataFromID(CardDB.cardIDEnum.EX1_575);//mana tide
 
         public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
         {
@@ -35,9 +35,21 @@ namespace HREngine.Bots
             int posi = (ownplay) ? p.ownMinions.Count : p.enemyMinions.Count;
 
             // if there is less than 2 basic totems already then assume non-basic mana tide to avoid being optimistic about the current board.
-            if (availa.Count > 2)
+            /*if (availa.Count > 2)
             {
                 p.callKid(kid6, posi, ownplay);
+                return;
+            }*/
+
+            if (availa.Contains(CardDB.cardIDEnum.CS2_050))
+            {
+                p.callKid(kid, posi, ownplay);
+                return;
+            }
+
+            if (availa.Contains( CardDB.cardIDEnum.NEW1_009))
+            {
+                p.callKid(kid3heal, posi, ownplay);
                 return;
             }
 
@@ -47,21 +59,10 @@ namespace HREngine.Bots
                 return;
             }
 
-            if (availa.Contains(CardDB.cardIDEnum.CS2_050))
-            {
-                p.callKid(kid, posi, ownplay);
-                return;
-            }
-
             if (availa.Contains( CardDB.cardIDEnum.CS2_051))
             {
                 p.callKid(kid4taunt, posi, ownplay);
                 
-                return;
-            }
-            if (availa.Contains( CardDB.cardIDEnum.NEW1_009))
-            {
-                p.callKid(kid3heal, posi, ownplay);
                 return;
             }
             
